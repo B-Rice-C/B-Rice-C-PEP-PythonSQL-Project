3944d4590511d922c7c3d86104101693314b6dfe
B-Rice-C <B-Rice-C@github.com>
2026-07-16 17:27:41 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-16T17:27:41.166Z

--- src/main/main.py
+++ src/main/main.py
@@ -51,19 +51,19 @@ def main():
 def load_and_clean_users(file_path):
 
     # load file
-    with open(file_path, "r") as user_data:
-    
-    # remove rows with to many or not enough colums
-    for line in user_data:
-        if line.count(,) not 1 or line[0] == "," or line[-1] == ",":
-            continue
-        seperator = line.index(",")
-        first_name = line[:seperator - 1]
-        last_name = line[seperator + 1:]
+    # with open(file_path, "r") as user_data:
 
-        # load data into database
-        cursor.execute("INSERT INTO users (firstName, lastName) VAULES (?,?)", (first_name, last_name))
-        conn.commit()
+    # # remove rows with to many or not enough colums
+    # for line in user_data:
+    #     if line.count(,) not 1 or line[0] == "," or line[-1] == ",":
+    #         continue
+    #     seperator = line.index(",")
+    #     first_name = line[:seperator - 1]
+    #     last_name = line[seperator + 1:]
+
+    #     # load data into database
+    #     cursor.execute("INSERT INTO users (firstName, lastName) VAULES (?,?)", (first_name, last_name))
+    #     conn.commit()
 
     print("TODO: load_users")
 
