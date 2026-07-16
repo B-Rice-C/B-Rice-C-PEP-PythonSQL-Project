9a63bf03f5fb9007c3adc49633c30cd8a07fbd81
B-Rice-C <B-Rice-C@github.com>
2026-07-16 21:08:58 +0000

    Test run: 0/4 passed, 0 failed, 0 skipped - 2026-07-16T21:08:58.866Z

--- src/main/main.py
+++ src/main/main.py
@@ -126,8 +126,11 @@ def write_user_analytics(csv_file_path):
         call_count[user_id] += 1
         total_duration[user_id] += duration
 
-        
-
+    with open (csv_file_path, "w") as file_analytics
+       file_analytics.write("userId,avgDuration,numCalls")
+       for user_id in total_duration:
+            average = duration[user_id] / call_count[user_id]            
+            file_analytics.write(user_id,average_duration,call_counts[user_id])
     print("TODO: write_user_analytics")
 
 
